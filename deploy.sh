90df275c9cf8c22d04383e835e35816b35a08c92
joe <joebenhur.f@gmail.com>
2023-04-24 19:35:47 +0530

    second commit

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ docker tag projectapp:v1 joeben21/prod:v1
 echo $docker | docker login --username joeben21 --password-stdin
 docker push joeben21/prod:v1
 
-scp -o StrictHostKeyChecking=no -i $Key docker-compose.yml ubuntu@35.77.223.33:/home/ubuntu/
-ssh -o StrictHostKeyChecking=no -i $Key ubuntu@35.77.223.33 << EOF
+scp -o StrictHostKeyChecking=no -i $Key docker-compose.yml ubuntu@13.233.172.93:/home/ubuntu/
+ssh -o StrictHostKeyChecking=no -i $Key ubuntu@13.233.172.93 << EOF
 
 echo $docker | docker login --username joeben21 --password-stdin
 docker-compose up -d
